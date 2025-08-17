820d755a4f2300758e5195878acb540f16e7e757
Jasec <jasec_7@hotmail.com>
2025-08-17 02:57:35 -0700

    complete p3 00 one to many lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,27 @@
-class Pet:
-    pass
 
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    def __init__(self, name, pet_type, owner = None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        if pet_type not in Pet.PET_TYPES:
+            raise ValueError("that is not a pet")
+        Pet.all.append(self)
+        
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+         return [pet for pet in Pet.all if pet.owner == self ]
+    
+    def add_pet(self, pet):
+        if not isinstance(pet,Pet):
+            raise TypeError('Pet must be instance of Pet class')
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key = lambda pet: pet.name)
+
